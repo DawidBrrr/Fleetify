30649636cbd48cd3314ce547f9fa3745669c0320
Dawid Brach <dbrach6@gmail.com>
2025-12-05 16:22:12 +0100

    Basic setup

---
+++ services/nginx-gateway/lua/sign.lua
@@ -0,0 +1,30 @@
+local hmac_lib = require "resty.hmac"
+local secret = os.getenv("INTERNAL_HMAC_SECRET")
+local request_id = ngx.var.request_id
+
+ngx.req.read_body()
+local body = ngx.req.get_body_data()
+
+if not body then
+    body = "{}"
+end
+
+if not secret then
+    ngx.log(ngx.ERR, "INTERNAL_HMAC_SECRET not set")
+    return
+end
+
+local hmac = hmac_lib:new(secret, hmac_lib.ALG_SHA256)
+if not hmac then
+    ngx.log(ngx.ERR, "Failed to create hmac object")
+    return
+end
+
+local signature = hmac:final(body)
+local signature_hex = hmac:to_hex(signature)
+
+ngx.req.set_header("X-Gateway-Signature", signature_hex)
+ngx.req.set_header("X-Request-ID", request_id)
+if not ngx.req.get_headers()["Content-Type"] then
+    ngx.req.set_header("Content-Type", "application/json")
+end
