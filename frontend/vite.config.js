b843d85b7c38c337b428c5b17ff74257971b57e7
Dawid Brach <dbrach6@gmail.com>
2026-01-09 21:07:43 +0100

    vite config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
+    host: '0.0.0.0', // Important for Docker to expose the server
+    port: 5173, // Standard vite port
+    strictPort: true, // Exit if the port is already in use
+    hmr: false, //Random shit with hmr maybe fix in future
+    watch: {
+      usePolling: true, 
+      interval: 1000
+    },
+    cors: true
   },
   preview: {
-    port: 4173,
-    open: true
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
   }
 });
